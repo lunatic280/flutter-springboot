3bea4a242aecf8f5cc9a9f184dab7f8a07ac476f
hongj <rick0201@naver.com>
2024-10-10 19:22:34 +0900

    현재 날짜에 맞는 메뉴를 가져오도록 변경

--- src/main/java/com/hagsig/Hagsig/HagsigService.java
+++ src/main/java/com/hagsig/Hagsig/HagsigService.java
@@ -1,6 +1,7 @@
 package com.hagsig.Hagsig;
 
 import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import org.springframework.cglib.core.Local;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,12 +9,18 @@ import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 import io.netty.handler.ssl.SslContextBuilder;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Service
 public class HagsigService {
 
     private final WebClient webClient;
 
     public HagsigService(WebClient.Builder webClientBuilder) {
+        //Todo 배포하기전에 인증서 검증하는지 확인
+        //Todo 안했으면 인즌서 검증하도록 변경
         // SSL 인증서를 검증하지 않도록 설정된 HttpClient 생성
         HttpClient httpClient = HttpClient.create()
                 .secure(spec -> spec.sslContext(SslContextBuilder.forClient()
@@ -27,7 +34,14 @@ public class HagsigService {
     }
 
     public String getJsonHagsig() {
-        String endpoint = "/api/widget/internalWidget/selectSikdan?SIKDAN_DT=20241004&SIKDANG_GB=2";
+
+        //현재 날짜 가져오기
+        LocalDateTime date = LocalDateTime.now();
+        //포맷지정 yyyymmdd
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        String InputDate = date.format(formatter);
+
+        String endpoint = String.format("/api/widget/internalWidget/selectSikdan?SIKDAN_DT=%s&SIKDANG_GB=2", InputDate);
         Mono<String> response = webClient.get()
                 .uri(endpoint)
                 .retrieve()
