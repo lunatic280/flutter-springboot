0db64f47353d6f15dba3dbc5c78d619019481292
hongj <rick0201@naver.com>
2024-10-10 20:24:06 +0900

    flutter 앱 주소 변경

--- src/main/java/com/hagsig/Hagsig/AppConfig.java
+++ src/main/java/com/hagsig/Hagsig/AppConfig.java
@@ -3,6 +3,8 @@ package com.hagsig.Hagsig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +12,19 @@ public class AppConfig {
     @Bean
     public WebClient.Builder webClientBuilder() {
         return WebClient.builder();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://10.0.2.2:56072")  // Flutter 앱 주소
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
     }
 }
